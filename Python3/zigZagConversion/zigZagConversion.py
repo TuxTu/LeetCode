2526db8d98d08cdb3390585c145223d24e42cda9
Tux <tux.tu@outlook.com>
2019-07-26 12:44:11 +0800

    zigZag

--- Python3/zigZagConversion/zigZagConversion.py
+++ Python3/zigZagConversion/zigZagConversion.py
@@ -1,5 +1,46 @@
+#92ms,13.9MB
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        ans = ""
+        if numRows == 1:
+            return s
+
+        rowsOfOutput = min(len(s), numRows)
+        ans = []
+        for i in range(rowsOfOutput):
+            ans.append("")
+
+        isDownward = 1
+        rows = 0
+        for ch in s:
+            ans[rows] += ch
+            rows += isDownward
+            if rows == 0 or rows == rowsOfOutput - 1:
+                isDownward *= -1
+
+        res = ""
+        for string in ans:
+            res += string
+
+        return res
+
+#84ms,13.8MB
+class Solution2:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+
         length = len(s)
-        for i in range(length):
+        circleLen = 2*numRows - 2
+        res = ""
+
+        for i in range(numRows):
+            j = 0
+            while i + j < length:
+                res += s[i + j]
+                if j + circleLen - i < length and i != 0 and i != numRows - 1:
+                    res += s[j + circleLen - i]
+                j += circleLen
+
+        return res
+
+print(Solution2().convert(input(), 3))
