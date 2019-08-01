a7f5486a4baba35ec1e47210c7a8f6980971d314
Tux <tux.tu@outlook.com>
2019-08-01 16:09:35 +0800

    regularExpressionMatching

---
+++ Python3/regularExpressionMatching/regularExpressionMatching.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+    	if not p:
+    		return not s
+    	
+    	firstMatch = bool(s) and (s[0] == p[0] or p[0] == '.')
+    	
+    	if len(p) >= 2 and p[1] == '*':
+    		return self.isMatch(s, p[2:]) or (firstMatch and self.isMatch(s[1:], p))
+    	else:
+    		return firstMatch and self.isMatch(s[1:], p[1:])
+
+print(Solution().isMatch(input(), input()))
