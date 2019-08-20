1a8d04729664f5e6807a2076b2c99f73bb663ac6
Tux <tux.tu@outlook.com>
2019-08-20 16:03:10 +0800

    removeNthFromEnd

--- Python3/removeNthFromEnd/removeNthFromEnd.py
+++ Python3/removeNthFromEnd/removeNthFromEnd.py
@@ -1,10 +1,10 @@
-#52ms,13.6MB
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
+#52ms,13.6MB
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
         nodeLength = 0
@@ -22,5 +22,25 @@ class Solution:
             temp = temp.next
         
         temp.next = temp.next.next
+        
+        return lazyHead.next
+
+#48ms,13.8MB
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        lazyHead = ListNode(0)
+        lazyHead.next = head
+        
+        firstP = lazyHead
+        secondP = lazyHead
+        
+        for i in range(n):
+            firstP = firstP.next
+        
+        while firstP.next != None:
+            firstP = firstP.next
+            secondP = secondP.next
+        
+        secondP.next = secondP.next.next
         
         return lazyHead.next
